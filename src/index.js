52af04b4b74f03d6768f7ebae09786aff06e4cf4
Jonathan Rodriguez Romero <21030021@itcelaya.edu.mx>
2024-05-01 23:57:39 -0600

    Final

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ app.use(express.urlencoded({extended: false}));
 
 //Routes
 app.use(require('./routes/index'))
-const PORT = process.env.PORT || 4000;
-app.listen(PORT);
-// eslint-disable-next-line no-console
-console.log("Server on port", PORT);
+
+app.listen(4000);
+console.log('Server on port 4000');
